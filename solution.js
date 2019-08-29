20592c5353f2ada750949605b0a094ddd4dd15cf
isalevine <isa.loves.coding@gmail.com>
2019-08-28 18:48:42 -0700

    solved

---
+++ solution.js
@@ -0,0 +1,47 @@
+// source: https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/559/
+
+
+
+// Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+
+// The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+
+// You may assume the integer does not contain any leading zero, except the number 0 itself.
+
+// Example 1:
+
+// Input: [1,2,3]
+// Output: [1,2,4]
+// Explanation: The array represents the integer 123.
+// Example 2:
+
+// Input: [4,3,2,1]
+// Output: [4,3,2,2]
+// Explanation: The array represents the integer 4321.
+
+
+
+
+var plusOne = function(digits) {
+    let lastIndex = digits.length - 1;
+    
+    checkTens(lastIndex);
+    
+    return digits;
+    
+    
+    function checkTens(index) {
+        if (digits[index] == 9) {       // make 0 and carry the 1
+            digits[index] = 0;
+            
+            if (digits[index - 1]) {    // if more digits to check, call recursively
+                checkTens(index - 1);
+            } else {
+                digits.unshift(1);
+            };
+            
+        } else {                        // add 1 to all other integers
+            digits[index]++;
+        };
+    };
+};
